930db8caeeec1028481e626f59e061d26fbe6b14
Randal <randal.t.meyer@gmail.com>
2026-02-05 17:12:04 -0800

    initial code checkin

--- build/index.js
+++ build/index.js
@@ -1,1 +1,6 @@
-"use strict";
+export * from "./Cache.js";
+export * from "./EphemeralBase.js";
+export * from "./EphemeralMap.js";
+export * from "./EphemeralSet.js";
+export * from "./HasCache.js";
+export * from "./uncache.js";
